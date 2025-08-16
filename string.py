7ddb5ad96a69f8a39da4fae9c25a9808b73b0a4b
Wildcat173 <Itsvehaanshukla@gmail.com>
2025-08-16 09:53:57 -0500

    Add files via upload

---
+++ string.py
@@ -0,0 +1,6 @@
+text = str(input("Enter a string: "))
+revtext = text[::-1]
+text = revtext
+
+print ("Reverse Of Given String Is")
+print (text)
---
+++ typecasting.py
@@ -0,0 +1,26 @@
+name="penguin"
+age= 15
+is_student = True
+weight = 38.5
+
+print("Name :", name)
+print("Data Type Of Name is", type(name))
+
+print("Age :", age)
+print("Data Type Of Age is", type(age))
+
+print("Weight :", weight)
+print("Data Type Of weight is", type(weight))
+
+print("is_student :", is_student )
+print("Data Type Of is_student  is", type(is_student ))
+
+age = str(age)
+print(age)
+print("Data Type of age is", type (age))
+weight = int(weight)
+print(weight)
+print("Data Type of Weight is", type (weight))
+
+
+
