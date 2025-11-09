07a19091dc29cb37850781f156a823e669003019
Wildcat173 <Itsvehaanshukla@gmail.com>
2025-11-09 08:51:31 -0600

    Add files via upload

---
+++ Secret.py
@@ -0,0 +1,14 @@
+class myClass:
+
+    # private variable
+    __privateVar = 27
+
+    def __privMeth(self):
+        print("I'm inside class myClass")
+
+    def hello(self):
+        print("Private Variable value:", myClass.__privateVar)
+
+foo = myClass()
+foo.hello()
+foo._privMeth()
